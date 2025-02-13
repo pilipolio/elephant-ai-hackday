65e1b35205ec70f6a6992291bdd4cd1be3d7860b
guillaume allain <guillaume.allain@elephant.healthcare>
2025-02-13 11:57:52 +0000

    basic chatgpt wrapper

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,31 @@
-
+import os
 import streamlit as st
-import pandas as pd
 
-st.title("🐘 Hack day is on!!! 🐘")
+from llama_index.llms.openai import OpenAI
+
+llm_name = "gpt-4o-mini"
+model_temperature = 0.8
+
+st.title("🐘 AI Immunisation/MCH Nurse 🐘")
+st.markdown(
+    (
+        "This demo allows you to query a LLM about immunisation and MCH topics."
+    )
+)
+
+query_tab, = st.tabs(["Query"])
+
+with query_tab:
+    st.subheader("Query")
+    st.markdown(
+        (
+            f"This is a simple wrapper around OpenAI's {llm_name} model."
+        )
+    )
+
+    query_text = st.text_input("Ask a question:")
+    if query_text:
+        with st.spinner("Generating answer..."):
+            llm = OpenAI(model=llm_name, temperature=model_temperature, api_key=os.environ["OPENAI_API_KEY"])
+            response = llm.complete(query_text)
+        st.markdown(str(response))
